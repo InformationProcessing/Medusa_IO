6dd641747fa373146b739e0f5208540106d1fb78
lollycraft <ngu.alexandra@gmail.com>
2022-02-23 18:25:23 +0000

    generalise commands for resizing the canvas, looping in progress

--- OOPSnake.py
+++ OOPSnake.py
@@ -10,9 +10,9 @@ root = tk.Tk()
 time1 = ''
 clock = Label(root)
 
-canvas_width = 1000
-canvas_height = 1000
-canvas = tk.Canvas(root, width = canvas_width, height = canvas_height,highlightthickness=3, highlightbackground="black")
+canvas_width = 700
+canvas_height = 700
+canvas = tk.Canvas(root, width = canvas_width+1, height = canvas_height+1,highlightthickness=10, highlightbackground="black")
 canvas.pack()
 
 class Snake:
@@ -34,7 +34,7 @@ class Snake:
         self.snake.append(canvas.create_rectangle(self.x,10*(len(self.snake)+1)+self.y,self.x+10,10*(len(self.snake)+1)+self.y+10,fill='gray'+str(len(self.snake)*2+1)))
         self.snakeblockscoordX.append(self.x) #vertical generation of the snake -> should look on how to make it general
         self.snakeblockscoordY.append(self.y-i*10)
-        print("initialise: ", i, "----",self.snakeblockscoordX[i], "- ",self.snakeblockscoordY[i])
+        # print("initialise: ", i, "----",self.snakeblockscoordX[i], "- ",self.snakeblockscoordY[i])
 
     def changedir(self,dir1):
         if dir1=='Up':
@@ -74,20 +74,15 @@ class Snake:
         self.snakeblockscoordY[0]=self.y
 
         for j in range(len(self.moveblocks)):
-            if self.snakeblockscoordX[j] < 0 or self.snakeblockscoordX[j] > 1000 or self.snakeblockscoordY[j] < 0 or self.snakeblockscoordY[j] > 1000:
-                if  self.snakeblockscoordX[j] < 0:     abs_move(1000,self.snakeblockscoordY[j],j)
-                if  self.snakeblockscoordX[j] > 1000:  abs_move(0,   self.snakeblockscoordY[j],j)
-                if  self.snakeblockscoordY[j] < 0:     abs_move(self.snakeblockscoordX[j],1000,j)
-                if  self.snakeblockscoordY[j] > 1000:  abs_move(self.snakeblockscoordX[j],   0,j)
+            if self.snakeblockscoordX[j] < 0 or self.snakeblockscoordX[j] > canvas_width or self.snakeblockscoordY[j] < 0 or self.snakeblockscoordY[j] > canvas_height:
+                if  self.snakeblockscoordX[j] < 0:              abs_move(canvas_width, self.snakeblockscoordY[j],j)
+                if  self.snakeblockscoordX[j] > canvas_width:   abs_move(0,            self.snakeblockscoordY[j],j)
+                if  self.snakeblockscoordY[j] < 0:              abs_move(self.snakeblockscoordX[j],canvas_height,j)
+                if  self.snakeblockscoordY[j] > canvas_height:  abs_move(self.snakeblockscoordX[j],            0,j)
             else:
                 print(j, "----",self.moveblocks[j][0], "- ",self.moveblocks[j][1])
                 canvas.move(self.snake[j],self.moveblocks[j][0],self.moveblocks[j][1])
             # print(self.moveblocks[0][0], "- ",self.moveblocks[0][1])
-
-        # if self.y < 0: self.y = 1000
-        # elif self.y > 1000: self.y = 0
-        # if self.x < 0: self.x = 1000
-        # elif self.x > 1000: self.x = 0
 
 
 
