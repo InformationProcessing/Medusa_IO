c0503a225c9d1e3c055da3acc59abaed36215a65
Václav Pavlíček <vaclav@pavlicek.cz>
2022-02-23 16:52:10 +0000

    feat(fpga-communication): add version 1 of the communication protocol to FPGACommunicator

--- visualiser/fpga_communicator.py
+++ visualiser/fpga_communicator.py
@@ -1,68 +1,75 @@
 import intel_jtag_uart
 import re
 
-ACC_FILTERED_READ_COMMAND = "r acc_fil"
-ACC_RAW_READ_COMMAND = "r acc_raw"
-START_COMMAND = "start"
-START_COMMAND_ACK = "start ack"
-END_COMMAND = "end"
-END_COMMAND_ACK = "end ack"
+READ_ACCPROC_COMMAND = "R ACCPROC"
+READ_ACCRAW_COMMAND = "R ACCRAW"
+READ_BUTTON_COMMAND = "R BUTTON"
+READ_SWITCH_COMMAND = "R SWITCH"
+WRITE_HEXTEXT = "W HEXTEXT"
+WRITE_LEDFLASH = "W LEDFLASH"
+
+ACCPROC_REGEX = r"K ACCPROC X([\da-fA-F])Y([\da-fA-F])Z([\da-fA-F]) ([\da-fA-F])"
+ACCRAW_REGEX = r"K ACCRAW X([\da-fA-F]+)Y([\da-fA-F]+)Z([\da-fA-F]+) ([\da-fA-F])"
+BUTTON_REGEX = r"K BUTTON ([\da-fA-F]+) ([\da-fA-F])"
+SWITCH_REGEX = r"K SWITCH ([\da-fA-F]) ([\da-fA-F])"
+HEXTEXT_REGEX = r"K HEXTEXT ([\da-fA-F])"
+LEDFLASH_REGEX = r"K LEDFLASH ([\da-fA-F])"
 
 
 class FPGACommunicator:
     def __init__(self):
-        self.is_alive = False
         try:
             self.ju = intel_jtag_uart.intel_jtag_uart()
 
         except Exception as e:
             print(e)
 
-    def start_comm(self):
-        self.ju.write((START_COMMAND + "\n").encode("utf-8"))
-        fpga_out = self.ju.read().decode("utf-8").replace("\n", "")
-        if fpga_out == START_COMMAND_ACK:
-            self.is_alive = True
-            return True
-        else:
-            return False
+    def __get_command_output(self, command):
+        self.ju.write((command + "\n").encode("utf-8"))
+        return self.ju.read().decode("utf-8")
 
-    def end_comm(self):
-        self.ju.write((END_COMMAND_ACK + "\n").encode("utf-8"))
-        fpga_out = self.ju.read().decode("utf-8").replace("\n", "")
-        if fpga_out == END_COMMAND_ACK:
-            self.is_alive = False
-            return True
-        else:
-            return False
+    def __read_value_and_error(self, command, regex):
+        fpga_out = self.__get_command_output(command)
+        response = {"value": 0, "error_code": 0}
+        re_match = re.match(regex, fpga_out)
+        if re_match is not None:
+            response["value"] = int(re_match.group(1), 16)
+            response["error_code"] = int(re_match.group(2), 16)
+        return response
 
-    def __read_acc_samples(self, command, samples):
-        self.ju.write((command + " " + str(samples) + "\n").encode("utf-8"))
-        fpga_out_lines = self.ju.read().decode("utf-8").split("\n")
+    def __read_acc(self, command, regex):
+        fpga_out = self.__get_command_output(command)
+        response = {"x": 0, "y": 0, "z": 0, "error_code": 0}
+        re_match = re.match(regex, fpga_out)
+        if re_match is not None:
+            response["x"] = int(re_match.group(1))
+            response["y"] = int(re_match.group(2))
+            response["z"] = int(re_match.group(3))
+            response["error_code"] = int(re_match.group(4))
+        return response
 
-        samples = []
+    def __write_request(self, command, value, regex):
+        final_command = command + " " + str(value) + "\n"
+        fpga_out = self.__get_command_output(final_command)
+        re_match = re.match(regex, fpga_out)
+        if re_match is not None:
+            return int(re_match.group(1), 16)
+        return -1
 
-        for line in fpga_out_lines:
-            re_match = re.match(r"acc: (\d+)", line)
-            if re_match is not None:
-                samples.append(int(re_match.group(1)))
-
-        return samples
-
-    def __read_acc(self, command):
-        samples = self.__read_acc_samples(command, 1)
-        if len(samples) == 1:
-            return samples[0]
-        return 0
-
-    def read_acc_filtered(self):
-        return self.__read_acc(ACC_FILTERED_READ_COMMAND)
-
-    def read_acc_filtered_samples(self, samples=5):
-        return self.__read_acc_samples(ACC_RAW_READ_COMMAND, samples)
+    def read_acc_proc(self):
+        return self.__read_acc(READ_ACCPROC_COMMAND, ACCPROC_REGEX)
 
     def read_acc_raw(self):
-        return self.__read_acc(ACC_RAW_READ_COMMAND)
+        return self.__read_acc(READ_ACCRAW_COMMAND, ACCRAW_REGEX)
 
-    def read_acc_raw_samples(self, samples=5):
-        return self.__read_acc_samples(ACC_RAW_READ_COMMAND, samples)
+    def read_button(self):
+        return self.__read_value_and_error(READ_BUTTON_COMMAND, BUTTON_REGEX)
+
+    def read_switch(self):
+        return self.__read_value_and_error(READ_SWITCH_COMMAND, SWITCH_REGEX)
+
+    def write_hextext(self, value):
+        return self.__write_request(WRITE_HEXTEXT, value, HEXTEXT_REGEX)
+
+    def write_ledflash(self, value):
+        return self.__write_request(WRITE_LEDFLASH, value, LEDFLASH_REGEX)
--- visualiser/main.py
+++ visualiser/main.py
@@ -2,12 +2,39 @@ from fpga_communicator import FPGACommunicator
 
 communicator = FPGACommunicator()
 
-if communicator.start_comm():
-    print("Communication started!")
-    for x in range(0, 10):
-        acc_value = communicator.read_acc_raw()
-        print("accelerometer value: " + str(acc_value))
-    communicator.end_comm()
-    print("Communication ended!")
-else:
-    print("Communication wasn't established")
+counter = 0
+sum = 0
+
+while True:
+    counter = counter + 1
+    acc_proc_reading = communicator.read_acc_proc()
+    print("accproc x: " + str(acc_proc_reading['x']) + " accproc y: " + str(acc_proc_reading['y']) + " accproc z: "
+          + str(acc_proc_reading['z']) + " error code: " + str(acc_proc_reading['error_code']))
+    acc_raw_reading = communicator.read_acc_raw()
+    print("accraw x: " + str(acc_raw_reading['x']) + " accraw y: " + str(acc_raw_reading['y']) + " accraw z: "
+          + str(acc_raw_reading['z']) + " error code: " + str(acc_raw_reading['error_code']))
+    button_response = communicator.read_button()
+    print("button value: " + str(button_response['value']) + " error_code " + str(button_response['error_code']))
+    switch_response = communicator.read_switch()
+    print("switch value: " + str(switch_response['value']) + " error_code " + str(switch_response['error_code']))
+    hextext_error_code = communicator.write_hextext(123)
+    print("hextext error code: " + str(hextext_error_code))
+    ledflash_error_code = communicator.write_ledflash(11)
+    print("ledflash error code: " + str(hextext_error_code))
+
+#    if counter == 3:
+#        value = sum // 3
+#        if value > 60:
+#            print("LEFT")
+#        elif value < -60:
+#            print("RIGHT")
+#        else:
+#            print("MIDDLE")
+#        sum = 0
+#        counter = 0
+
+#acc_value = acc_reading['x']
+#if acc_value > 200:
+#    acc_value = acc_value - 4294967295
+#acc_value = acc_value + 20
+#sum = sum + acc_value
