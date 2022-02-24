4adfa7d124ecfc183edd7828709b691f12eac5b6
lollycraft <ngu.alexandra@gmail.com>
2022-02-24 18:15:49 +0000

    food added for increase in size by 1 + annimation in progress

--- OOPSnake.py
+++ OOPSnake.py
@@ -32,8 +32,8 @@ class Snake:
             self.addblock(i)
 
     def addblock(self,i):
-        self.moveblocks.append([0,-10])
-        block=tk.Canvas(canvas,width=10, height=10, bd=0, highlightthickness=0, relief='ridge', bg="gray"+str(i*3))
+        self.moveblocks.append([0,0])
+        block=tk.Canvas(canvas,width=10, height=10, bd=0, highlightthickness=0.5, highlightbackground="white", relief='ridge', bg="gray"+str(i*3))
         block.place(x=self.x, y=self.y-i*10)
         self.snake.append(block)
         self.snakeblockscoordX.append(self.x)
@@ -51,6 +51,7 @@ class Snake:
             self.move = [10,0]
         
     def movesnake(self):
+        #special teleportation of a block
         def abs_move(new_x, new_y,j):
             self.snake[j].place(x=new_x, y=new_y)
             self.snakeblockscoordX[j] = new_x
@@ -58,36 +59,35 @@ class Snake:
             self.x = self.snakeblockscoordX[0]
             self.y = self.snakeblockscoordY[0]
 
+        #passing on block movement orientation.step to the following block
         for j in range(len(self.moveblocks)-1,0,-1):
             self.moveblocks[j]=self.moveblocks[j-1]
             self.snakeblockscoordX[j]=self.snakeblockscoordX[j-1]
             self.snakeblockscoordY[j]=self.snakeblockscoordY[j-1]
             print("pos:", j, "----",self.snakeblockscoordX[j], ": ",self.snakeblockscoordY[j])
         self.moveblocks[0]=self.move
+        #blocking moving backwards and overlapping snake
         if self.moveblocks[0][0] == -self.moveblocks[1][0]:
             self.moveblocks[0][0] = self.moveblocks[1][0]
         if self.moveblocks[0][1] == -self.moveblocks[1][1]:
             self.moveblocks[0][1] = self.moveblocks[1][1]
 
         print(self.x, " ", self.y)
+        #for every block in the snake, move or teleport
         for j in range(len(self.snake)):
             if self.snakeblockscoordX[j] < 10 or self.snakeblockscoordX[j] > canvas_width or self.snakeblockscoordY[j] < 10 or self.snakeblockscoordY[j] > canvas_height:
-                if  self.snakeblockscoordX[j] < 10:              abs_move(canvas_width, self.snakeblockscoordY[j],j)
+                if  self.snakeblockscoordX[j] < 10:             abs_move(canvas_width, self.snakeblockscoordY[j],j)
                 if  self.snakeblockscoordX[j] > canvas_width:   abs_move(10,            self.snakeblockscoordY[j],j)
-                if  self.snakeblockscoordY[j] < 10:              abs_move(self.snakeblockscoordX[j],canvas_height,j)
+                if  self.snakeblockscoordY[j] < 10:             abs_move(self.snakeblockscoordX[j],canvas_height,j)
                 if  self.snakeblockscoordY[j] > canvas_height:  abs_move(self.snakeblockscoordX[j],            10,j)
             else:
-                print(j, "----",self.moveblocks[j][0], "- ",self.moveblocks[j][1])
+                # print(j, "----",self.moveblocks[j][0], "- ",self.moveblocks[j][1])
                 self.snake[j].place(x=self.snakeblockscoordX[j], y=self.snakeblockscoordY[j])
 
         self.x = self.x + int(self.moveblocks[0][0])
         self.y = self.y + int(self.moveblocks[0][1])
         self.snakeblockscoordX[0]=self.x
         self.snakeblockscoordY[0]=self.y
-
-
-
-
 
     def adjustspeed(self,speed):
         self.speed = speed
@@ -96,8 +96,29 @@ class Snake:
         return self.speed
 
 
-player = Snake(500, 500)
+class powerup:
+        xcoord = 0
+        ycoord = 0
+        id = 0 #= canvas.create_oval(500,500,500+6,500+6,fill='blue',tag="test")
+        #the id of food is a necessary parameter to have as it can be called
+        #simply can just create a new oval or new parameter rather than whole new class
+        def __init__(self):
+            x = random.randrange(30,500,10)
+            y = random.randrange(20,500,10) #using random num_gen for food. 
+            self.id = canvas.create_oval(x,y+5,x+5,y+10,fill='blue',tag="test")
+            self.xcoord=x
+            self.ycoord=y
+        def move(self):
+            x = random.randrange(30,500,10)
+            y = random.randrange(20,500,10) #using random num_gen for food.
+            canvas.delete("test")
+            self.id = canvas.create_oval(x,y+5,x+5,y+10,fill='blue',tag="test")
+            self.xcoord=x
+            self.ycoord=y
 
+
+player = Snake(500, 500)
+food = powerup()
 
 def kpress(event):
       if event.keysym == 'Up':
@@ -121,20 +142,47 @@ def decreasespeed():
     if player.getspeed()!=1:
         player.adjustspeed(player.getspeed()-1)
 
+def snakeAnnimation( player,animation ):
+    if animation == "blue":
+        for j in range(len(player.snake)):
+            player.snake[j].configure(bg="blue")
+    elif animation == "return":
+        for j in range(len(player.snake)):
+            player.snake[j].configure(bg="gray"+str(j*3))
+
+
 button1 = tk.Button(text='Increase Speed',command=increasespeed)
 canvas.create_window(270,18,window=button1)
 button2 = tk.Button(text='Decrease Speed',command=decreasespeed)
 canvas.create_window(100,18,window=button2)
 
-def tick():
+def tick(player):
     global time1
     time2 = time.strftime('%H:%M:%S')
     if time2 != time1:
             time1 = time2
             clock.config(text=time2)
     player.movesnake()
-    clock.after(int(100/(player.getspeed())),tick)
+
+    global food
+    if (player.x == food.xcoord) and (player.y == food.ycoord):
+          print("Player's pos:",player.x,player.y)
+          print("Food Pos: ",food.xcoord,food.ycoord) 
+          food.move()
+          player.addblock(len(player.snake)+1)
+          animation = "blue"
+          snakeAnnimation(player,animation)
+          animation = "return"
+          snakeAnnimation(player,animation)
+          clock.after(int(100/player.getspeed()),lambda: tick(player))
+      
+    else:
+        # animation = "return"
+        # snakeAnnimation(player,animation)
+        clock.after(int(100/player.getspeed()),lambda: tick(player))
+
+
 
 root.bind("<Key>",kpress)
-tick()
+tick(player)
 root.mainloop()
