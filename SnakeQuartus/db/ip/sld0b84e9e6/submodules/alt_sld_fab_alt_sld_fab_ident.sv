c9ef79e397d7d91fdc9ebef71575c9813d452dfb
Michal Palic <michalpalic1@gmail.com>
2022-02-26 15:05:14 +0000

    Big update - most features supported

--- SnakeQuartus/db/ip/sld0b84e9e6/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ SnakeQuartus/db/ip/sld0b84e9e6/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 1f814f9d84873ed5e37e
+//   DESIGN_HASH   : 08cd23af875945ae2600
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h1f814f9d84873ed5e37e };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h08cd23af875945ae2600 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
