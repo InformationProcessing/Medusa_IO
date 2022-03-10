d63ea339ceba99e9a588bb75ee1dd76078b31171
matthew-setiawan <94852286+matthew-setiawan@users.noreply.github.com>
2022-03-10 14:57:20 +0000

    Update SnakeTCPServer.py

--- Synced Food/SnakeTCPServer.py
+++ Synced Food/SnakeTCPServer.py
@@ -1,6 +1,33 @@
 import socket
 import random
 
+def checkcollision():
+    global msg1, msg2, gamestate
+    array1 = []
+    coord1 = msg1.split('|')[0]
+    temparray = coord1.split(';')
+    for i in range(len(temparray)):
+        if len(temparray[i].split(","))>1:
+            array1.append([temparray[i].split(",")[0],temparray[i].split(",")[1]])
+    array2 = []
+    coord2 = msg2.split('|')[0]
+    temparray = coord2.split(';')
+    for i in range(len(temparray)):
+        if len(temparray[i].split(","))>1:
+            array2.append([temparray[i].split(",")[0],temparray[i].split(",")[1]])
+    head1 = array1[0]
+    head2 = array2[0]
+    for i in range(1,len(array1)):
+        print(head2,array1[i])
+        if abs(int(head2[0])-int(array1[i][0]))<10 and abs(int(head2[1])-int(array1[i][1]))<10:
+            gamestate = 'msg1won'
+            return 0
+    for i in range(1,len(array2)):
+        if abs(int(head1[0])-int(array2[i][0]))<10 and abs(int(head1[1])-int(array2[i][1]))<10:
+            gamestate = 'msg2won'
+            return 0
+    gamestate = 'none'
+
 time1 = ''
 
 server_port = 12001
@@ -12,6 +39,7 @@ c2add = ('localhost',14000)
 
 msg1 = ""
 msg2 = ""
+gamestate = 'none'
 
 #nextcoord = random.randrange(30,500,10)+','+random.randrange(30,500,10)+','+random.randint(0,3)+''
 
@@ -20,7 +48,17 @@ while 1:
     cmsg = cmsg.decode()
     if cadd[1]==13000:
         msg1 = cmsg
-        welcome_socket.sendto(msg1.encode(), c2add)
+        if msg1 != "" and msg2 != "":
+            checkcollision()
+        if gamestate == 'none':
+            welcome_socket.sendto(msg1.encode(), c2add)
+        else:
+            break
     else:
         msg2 = cmsg
-        welcome_socket.sendto(msg2.encode(), c1add)
+        if msg1 != "" and msg2 != "":
+            checkcollision()
+        if gamestate == 'none':
+            welcome_socket.sendto(msg2.encode(), c1add)
+        else:
+            break
