2151f2e62404ac6e7dd135a0873ccdb16e17e4f5
matthew-setiawan <94852286+matthew-setiawan@users.noreply.github.com>
2022-03-15 15:42:08 +0000

    Add files via upload

---
+++ SnakeTCPClient/New/SnakeTCPClient2.py
@@ -0,0 +1,136 @@
+import tkinter as tk
+from tkinter import *
+import time
+import sys
+import random
+import os
+import socket
+import SnakeGameMap
+import subprocess
+import sys
+
+my_port = 14000
+time1 = ''
+clock = Label(SnakeGameMap.root)
+
+otherplayer = []
+otherplayerblocks=[]
+
+T = tk.Text(SnakeGameMap.root, height=2, width=15)
+T.pack()
+T.insert(tk.END, "Client 2")
+T.place(x = 350,y = 35)
+
+def spawn_program_and_die(program, exit_code=0):
+    subprocess.Popen(program)
+    sys.exit(exit_code)
+
+def mergearray(array1,array2):
+      newarray=[]
+      for i in range(len(array1)):
+            newarray.append([array1[i]+100,array2[i]+100])
+      return newarray
+
+player = SnakeGameMap.player
+food = SnakeGameMap.food
+wefoundfood = 0
+_x = 0
+_y = 0
+_r = 0
+_j = 0
+
+def sendCoord():
+      global my_port, wefoundfood
+      msg = ""
+      for i in range(len(player.snakeblockscoordX)):
+            msg += str(player.snakeblockscoordX[i]) + "," + str(player.snakeblockscoordY[i]) + ";"
+      msg = msg + "|" + str(_x) + "," + str(_y) + "," + str(_r) + "," + str(_j) + "," + str(wefoundfood)
+      wefoundfood = 0
+      client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      client_socket.bind(('',my_port))
+      client_socket.sendto(msg.encode(),('localhost',12001))
+      msg, sadd = client_socket.recvfrom(2048)
+            
+      msg = msg.decode().split('|')
+      print(msg)
+      
+      if len(msg)<2:
+            f = open("New/client2.txt","w")
+            f.write("Score: "+str(len(player.snakeblockscoordX))+"\n"+"You "+["Won","Lost"][(msg[0][3]!="2")])
+            f.close()
+            spawn_program_and_die(["python3","New/Launcher2.py"])
+      
+      foodinfo = msg[1].split(",")
+      if foodinfo[4] == '1':
+            global food
+            food.delete(int(foodinfo[3]))
+            food.generate(int(foodinfo[0]),int(foodinfo[1]),int(foodinfo[2]))
+            food.generate(int(foodinfo[0]),int(foodinfo[1]),int(foodinfo[2]))
+      
+      return msg[0]
+
+def deleteblocks():
+      global otherplayerblocks
+      for i in range(len(otherplayerblocks)):
+          SnakeGameMap.canvas.delete(otherplayerblocks[i])
+
+def updateothers():
+      global otherplayer
+      global otherplayerblocks
+      for i in range(len(otherplayer)):
+              for j in range(len(otherplayer[i])):
+                    otherplayerblocks.append(SnakeGameMap.canvas.create_rectangle(int(otherplayer[i][j][0]),int(otherplayer[i][j][1]),int(otherplayer[i][j][0])+10,int(otherplayer[i][j][1])+10,fill='violetred1'))
+      
+
+def tick(player,found):
+      
+    deleteblocks()    
+    global time1
+    global food
+    global otherplayer
+    global otherplayerblocks
+    global wefoundfood
+    global _x,_y,_r,_j
+    time2 = time.strftime('%H:%M:%S')
+    
+    msg = sendCoord()
+    
+    array = []
+    temparray = msg.split(";")
+    
+    for i in range(len(temparray)):
+        if len(temparray[i])>2:
+            array.append([temparray[i].split(",")[0],temparray[i].split(",")[1]])
+    
+    otherplayer=[array]
+
+    if time2 != time1:
+            time1 = time2
+            clock.config(text=time2)
+    player.movesnake()
+    
+    updateothers()
+    
+    for j in range (len(food.power_ups)):
+        if (player.x == food.power_upsX[j]) and (player.y == food.power_upsY[j]):
+            player.adjustspeed(1)
+            food.powerupType(player,food.power_ups[j][1])
+            clock.after(100,lambda: tick(player,FALSE)) 
+            food.delete(j)
+            _x = random.randrange(30,500,10)
+            _y = random.randrange(20,500,10)
+            _r = random.randint(0,3)
+            _j = j
+            food.generate(_x,_y,_r)
+            food.generate(_x,_y,_r)
+            found = TRUE
+            wefoundfood = 1
+            break
+
+    if found == FALSE : clock.after(int(30/player.getspeed()),lambda: tick(player,FALSE))
+
+
+
+SnakeGameMap.root.bind("<Key>",SnakeGameMap.kpress)
+tick(player,FALSE)
+SnakeGameMap.root.mainloop()
---
+++ SnakeTCPClient/New/SnakeTCPServer.py
@@ -0,0 +1,67 @@
+import socket
+import random
+
+def checkcollision():
+    global msg1, msg2, gamestate
+    array1 = []
+    coord1 = msg1.split('|')[0]
+    temparray = coord1.split(';')
+    for i in range(len(temparray)):
+        if len(temparray[i].split(","))>1:
+            array1.append([temparray[i].split(",")[0],temparray[i].split(",")[1]])
+    array2 = []
+    coord2 = msg2.split('|')[0]
+    temparray = coord2.split(';')
+    for i in range(len(temparray)):
+        if len(temparray[i].split(","))>1:
+            array2.append([temparray[i].split(",")[0],temparray[i].split(",")[1]])
+    head1 = array1[0]
+    head2 = array2[0]
+    for i in range(1,len(array1)):
+        print(head2,array1[i])
+        if abs(int(head2[0])-int(array1[i][0]))<5 and abs(int(head2[1])-int(array1[i][1]))<5:
+            gamestate = 'msg1won'
+            return 0
+    for i in range(1,len(array2)):
+        if abs(int(head1[0])-int(array2[i][0]))<5 and abs(int(head1[1])-int(array2[i][1]))<5:
+            gamestate = 'msg2won'
+            return 0
+    gamestate = 'none'
+
+time1 = ''
+
+server_port = 12001
+welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+welcome_socket.bind(('',server_port))
+
+c1add = ('localhost',13000)#146.169.165.26
+c2add = ('localhost',14000)
+
+msg1 = ""
+msg2 = ""
+gamestate = 'none'
+result = ""
+
+#nextcoord = random.randrange(30,500,10)+','+random.randrange(30,500,10)+','+random.randint(0,3)+''
+
+while 1:
+    cmsg, cadd = welcome_socket.recvfrom(2048)  	
+    cmsg = cmsg.decode()
+    if cadd[1]==13000:
+        msg1 = cmsg
+        if msg1 != "" and msg2 != "":
+            checkcollision()
+        if gamestate == 'none':
+            welcome_socket.sendto(msg1.encode(), c2add)
+        else:
+            welcome_socket.sendto(gamestate.encode(), c2add)
+            welcome_socket.sendto(gamestate.encode(), c1add)
+    else:
+        msg2 = cmsg
+        if msg1 != "" and msg2 != "":
+            checkcollision()
+        if gamestate == 'none':
+            welcome_socket.sendto(msg2.encode(), c1add)
+        else:
+            welcome_socket.sendto(gamestate.encode(), c2add)
+            welcome_socket.sendto(gamestate.encode(), c1add)
