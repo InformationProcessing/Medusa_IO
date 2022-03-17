c74135d584e8a39c67daa454d9437c91e312d715
Václav Pavlíček <vaclav@pavlicek.cz>
2022-03-17 23:27:57 +0000

    add buttons for changing orders in the global leader board

--- SnakeVisualiser/components/global_leaderboard.py
+++ SnakeVisualiser/components/global_leaderboard.py
@@ -6,17 +6,24 @@ from SnakeVisualiser.components.scrollable_frame import ScrollableFrame
 class GlobalLeaderboard:
     def __init__(self, root):
         self.root = root
-        self.title_label = None
-        self.__set_title_label("Global leaderboard")
-        self.leaderboard_frame = ttk.Frame(root, padding="0 0 0 0")
-        self.leaderboard_frame.grid(column=0, row=1, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
+        self.title_label = None
+        self.__set_title_label("Global leaderboard")
+        self.buttons_frame = ttk.Frame(root, padding="0 0 0 0")
+        self.buttons_frame.grid(column=0, row=1, sticky=(N, W, E, S))
+        self.__create_change_order_button("Sort by total score", self.sort_by_total_score)
+        self.leaderboard_frame = ttk.Frame(root, padding="0 0 0 0")
+        self.leaderboard_frame.grid(column=0, row=2, sticky=(N, W, E, S))
 
         self.scrollable_frame = ScrollableFrame(self.leaderboard_frame)
         self.fetched_players = self.__fetch_players()
         self.clean_leaderboard()
-        self.show_players_by_the_highest_score()
+        self.sort_by_highest_score()
+
+    def __create_change_order_button(self, text, command):
+        self.change_order_button = Button(self.buttons_frame, text=text, command=command)
+        self.change_order_button.grid(column=0, row=0, sticky=(N, W, E, S))
 
     def __set_title_label(self, text):
         if self.title_label is not None:
@@ -73,8 +80,21 @@ class GlobalLeaderboard:
         for child in self.scrollable_frame.scrollable_frame.winfo_children():
             child.destroy()
 
-    def show_players_by_the_highest_score(self):
+    def sort_by_total_score(self):
         self.clean_leaderboard()
+        self.change_order_button.destroy()
+        self.__create_change_order_button("Sort by highest score", self.sort_by_highest_score)
+        self.__set_title_label("Global leaderboard - total score")
+        sorted_list = sorted(self.fetched_players, key=lambda player: player['totalscore'], reverse=True)
+        for player in sorted_list:
+            ttk.Label(self.scrollable_frame.scrollable_frame,
+                      text=f"{player['username']} - {player['totalscore']}").pack()
+        self.scrollable_frame.pack()
+
+    def sort_by_highest_score(self):
+        self.clean_leaderboard()
+        self.change_order_button.destroy()
+        self.__create_change_order_button("Sort by total score", self.sort_by_total_score)
         self.__set_title_label("Global leaderboard - highest score")
         sorted_list = sorted(self.fetched_players, key=lambda player: player['highestscore'], reverse=True)
         for player in sorted_list:
